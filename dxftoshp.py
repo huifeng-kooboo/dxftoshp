8cba389b7202c3e06011bd93354f8e2f379089c5
Kim Yong Hyun <queenmedley@gmail.com>
2019-02-18 00:12:51 +0900

    second commit

---
+++ dxftoshp.py
@@ -0,0 +1,33 @@
+# This is for extracting contour height from Korean dxf map
+
+# This use ezdxf moulde. https://pypi.org/project/ezdxf/0.6.2/
+
+import ezdxf
+
+def print_entity(e):
+    print("LINE on layer : %s\n" % e.dxf.layer)
+    print("start point : %s\n" % e.dxf.start)
+    print("end point : %s\n" % e.dxf.end)
+
+# Read file name first, this example use one file name
+# modify later
+
+dwg=ezdxf.readfile("7111_7114.dxf")
+
+# if file not exit print error message then exit
+if dwg == 0 :
+    print("file not exist")
+    exit(1)
+
+# there are three diffrent layout model space, paper spae, block
+# extract data from model space layout and assige data to dxf entity,msp.
+msp=dwg.modelspace()
+
+# Korean contour is consisted of LWPOLYLINE, and layer name is F0017111,F0017114
+# Read each entity 'e', 
+for e in msp:
+    if e.dxftype()=='LWPOLYLINE':
+        print(e.dxf.elevation)
+
+
+
