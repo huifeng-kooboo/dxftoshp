ed2c6538e09209e2eedf42236cd8b8ed4894f762
Kim YongHyun <queenmedley@gmail.com>
2019-02-27 18:43:41 +0900

    start qt5 interface

---
+++ test/test_dxftoshp_dialog.py
@@ -0,0 +1,55 @@
+# coding=utf-8
+"""Dialog test.
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
+__author__ = 'queenmedley@gmail.com'
+__date__ = '2019-02-27'
+__copyright__ = 'Copyright 2019, Kim Yonghyun'
+
+import unittest
+
+from PyQt5.QtGui import QDialogButtonBox, QDialog
+
+from dxftoshp_dialog import dxftoshpDialog
+
+from utilities import get_qgis_app
+QGIS_APP = get_qgis_app()
+
+
+class dxftoshpDialogTest(unittest.TestCase):
+    """Test dialog works."""
+
+    def setUp(self):
+        """Runs before each test."""
+        self.dialog = dxftoshpDialog(None)
+
+    def tearDown(self):
+        """Runs after each test."""
+        self.dialog = None
+
+    def test_dialog_ok(self):
+        """Test we can click OK."""
+
+        button = self.dialog.button_box.button(QDialogButtonBox.Ok)
+        button.click()
+        result = self.dialog.result()
+        self.assertEqual(result, QDialog.Accepted)
+
+    def test_dialog_cancel(self):
+        """Test we can click cancel."""
+        button = self.dialog.button_box.button(QDialogButtonBox.Cancel)
+        button.click()
+        result = self.dialog.result()
+        self.assertEqual(result, QDialog.Rejected)
+
+if __name__ == "__main__":
+    suite = unittest.makeSuite(dxftoshpDialogTest)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
+
